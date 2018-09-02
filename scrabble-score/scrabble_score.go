77c32ebde2ce482cc8ef5e1727c7ccf4418b44d4
Jason Voigt <joyrexus@icloud.com>
2018-09-02 10:15:31 -0400

    Use letter-to-point map for scoring

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -2,27 +2,41 @@ package scrabble
 
 import "strings"
 
-// rules is a mapping from letter ranges to points.
-var rules = map[string]int{
-	"AEIOULNRST": 1,
-	"DG":         2,
-	"BCMP":       3,
-	"FHVWY":      4,
-	"K":          5,
-	"JX":         8,
-	"QZ":         10,
+// a mapping of letters to their scrabble score
+var letters = map[string]int{
+	"A": 1,
+	"E": 1,
+	"I": 1,
+	"O": 1,
+	"U": 1,
+	"L": 1,
+	"N": 1,
+	"R": 1,
+	"S": 1,
+	"T": 1,
+	"D": 2,
+	"G": 2,
+	"B": 3,
+	"C": 3,
+	"M": 3,
+	"P": 3,
+	"F": 4,
+	"H": 4,
+	"V": 4,
+	"W": 4,
+	"Y": 4,
+	"K": 5,
+	"J": 8,
+	"X": 8,
+	"Q": 10,
+	"Z": 10,
 }
 
 // Score computes a word's scrabble score.
 func Score(word string) int {
 	total := 0
 	for _, c := range word {
-		for letters, points := range rules {
-			if strings.Contains(letters, strings.ToUpper(string(c))) {
-				total = total + points
-				break
-			}
-		}
+		total += letters[strings.ToUpper(string(c))]
 	}
 	return total
 }
