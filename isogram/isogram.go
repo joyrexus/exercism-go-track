8990aa2eea3ea8c8c3bd22c2c1218f73873a7993
Jason Voigt <joyrexus@icloud.com>
2018-09-03 09:18:02 -0400

    Add isogram solution

---
+++ isogram/isogram.go
@@ -0,0 +1,21 @@
+package isogram
+
+import (
+	"fmt"
+	"strings"
+)
+
+// IsIsogram tests whether a string is an isogram.
+func IsIsogram(s string) bool {
+	var chars = map[rune]int{}
+	for _, r := range strings.ToLower(s) {
+		if strings.Contains(" -", string(r)) {
+			continue
+		}
+		if chars[r] == 1 {
+			return false
+		}
+		chars[r] = 1
+	}
+	return true
+}
