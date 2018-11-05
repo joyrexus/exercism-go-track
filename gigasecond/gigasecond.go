3e9025523d69fc01624f5501205a42dcceaceb65
Jason Voigt <joyrexus@icloud.com>
2018-11-04 22:56:54 -0500

    Simplify solution

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -4,5 +4,5 @@ import "time"
 
 // AddGigasecond returns a time that's 10^9 seconds later than the given time.
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Second * time.Duration(1e09))
+	return t.Add(time.Second * 1e9)
 }
