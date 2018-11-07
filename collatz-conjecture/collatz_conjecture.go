97d5ae59817a0b460044936438118614972b5d2f
Jason Voigt <joyrexus@icloud.com>
2018-11-07 07:44:37 -0500

    Simplify solution

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -5,21 +5,18 @@ import "errors"
 // CollatzConjecture returns the number of steps required to reach 1 when
 // applying the collatz algorithm on the given input.
 func CollatzConjecture(n int) (int, error) {
-	steps := 0
-	for {
-		if n < 1 {
-			return steps, errors.New("input must be a positive integer")
-		}
-		if n == 1 {
-			return steps, nil
-		}
+	if n < 1 {
+		return 0, errors.New("input must be a positive integer")
+	}
 
+	var steps int
+	for n > 1 {
 		steps++
-
 		if n%2 == 0 {
 			n = n / 2
 		} else {
 			n = (3 * n) + 1
 		}
 	}
+	return steps, nil
 }
