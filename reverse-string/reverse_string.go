066e1578806ff4c750e0d5865fcb25d5674b0093
Jason Voigt <joyrexus@icloud.com>
2018-11-09 21:08:15 -0500

    Tweak solution

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -3,9 +3,8 @@ package reverse
 // String reverses a string.
 func String(s string) string {
 	r := []rune(s)
-	end := len(r) - 1 // index of final rune
-	for i := len(r)/2-1; i >= 0; i-- {
-		r[i], r[end-i] = r[end-i], r[i]
+	for i, j := len(r)/2-1, len(r) - 1; i >= 0; i-- {
+		r[i], r[j-i] = r[j-i], r[i]
 	}
 	return string(r)
 }
