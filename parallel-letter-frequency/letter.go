a89b5dbfbb7e824c7d12373b4a0b4a84c84e8bbc
Jason Voigt <joyrexus@icloud.com>
2018-11-16 08:06:58 -0500

    Fix race condition

--- parallel-letter-frequency/letter.go
+++ parallel-letter-frequency/letter.go
@@ -1,31 +1,23 @@
 package letter
 
-import "sync"
-
 // ConcurrentFrequency finds the frequency of letters in an array of strings.
 func ConcurrentFrequency(texts []string) FreqMap {
-	var wg sync.WaitGroup
-	counts := make(chan FreqMap) // channel to send counts for each text
+	counts := make(chan FreqMap, len(texts))
 	totals := FreqMap{}
 
 	// map: get freq counts for each text concurrently
 	for _, text := range texts {
-		wg.Add(1)
 		go func(text string) {
-			defer wg.Done()
 			counts <- Frequency(text)
 		}(text)
 	}
 
 	// reduce: tally up the counts as they're sent on the channel
-	go func() {
-		for c := range counts {
-			for k, v := range c {
-				totals[k] += v
-			}
+	for range texts {
+		for k, v := range <-counts {
+			totals[k] += v
 		}
-	}()
+	}
 
-	wg.Wait()
 	return totals
 }
