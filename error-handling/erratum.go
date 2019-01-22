1b81df1841aa10598d75983146065167f7ec5b91
Jason Voigt <joyrexus@icloud.com>
2019-01-22 08:05:41 -0500

    Improve error-handling solution

--- error-handling/erratum.go
+++ error-handling/erratum.go
@@ -3,20 +3,17 @@ package erratum
 // Use uses an opener to open a resource then calls the resource's
 // Frob method with the provided input.
 func Use(o ResourceOpener, input string) (err error) {
-	var res Resource
-	for {
-		if res, err = o(); err == nil {
-			break
-		}
+	res, err := o()
+	for err != nil {
 		if _, ok := err.(TransientError); !ok {
 			return err
 		}
+		res, err = o()
 	}
 	defer func() {
 		if r := recover(); r != nil {
-			if ferr, ok := r.(FrobError); ok {
-				res.Defrob(ferr.defrobTag)
-				err = ferr
+			if err, ok := r.(FrobError); ok {
+				res.Defrob(err.defrobTag)
 			}
 			err = r.(error)
 		}
